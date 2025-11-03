113ef6e3362d29a2ef8ade9c47be7574b037e2f2
horrorbringer <vannysmilekh@gmail.com>
2025-11-03 08:34:47 +0700

    update router

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+interface IAppProps {
+    
+}
+
+export default function About (props: IAppProps) {
+  return (
+    <div>
+      About Page
+    </div>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+interface IAppProps {
+
+}
+
+export default function Home (props: IAppProps) {
+  return (
+    <div>
+      Hompage
+    </div>
+  );
+}
